04182bdef9dd2bece1614bc142047c0e03acf84b
padas2 <Supreeth.Padavala@in.pega.com>
2019-05-15 00:52:26 +0530

    Making it more user friendly

--- bitbucket-decline-prs-multiple.sh
+++ bitbucket-decline-prs-multiple.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
-for i in `seq 1 100`; do 
+function printHelp {
+	echo "-projectKey				: Project Key"
+	echo "-repository				: Full Repository name"
+	echo "-userSlug				: User Slug or short user id"
+	echo "-password				: Self explanatory"
+	echo "-bitbucketServerUrl			: Bitbucket Server Url "
+	echo "-fromValue				: First Id of List of PRs to be declined"
+	echo "-toValue				: Final Id of Pr to be declined"
+}
+
+if [ $# -eq 0 ]
+  then
+	echo ""
+	echo "Since no arguments have been passed, printing help"
+	echo "" 
+	echo ""
+    printHelp
+	exit 1
+fi
+
+trap "exit" INT
+for i in `seq 1080 1200`; do 
 	echo "Declining Pull request $i "
-	curl -u repo_admin_user:repo_admin_password -X POST "http://localhost:7990/rest/api/1.0/projects/${projectKey}/repos/${repositorySlug}/pull-requests/$i/decline?version=1" -H "X-Atlassian-Token: no-check"
+	curl -u "plugintester:Plugintester123" -X POST "https://git.dev.pega.io/rest/api/1.0/projects/LOADTEST/repos/prpc-platform/pull-requests/$i/decline?version=0" -H "X-Atlassian-Token: no-check"
 done
 
