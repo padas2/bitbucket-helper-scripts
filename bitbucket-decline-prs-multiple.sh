942ea2b38a202d48483a47ca7dce12a8f2acbf0a
padas2 <Supreeth.Padavala@in.pega.com>
2019-05-15 03:02:13 +0530

    Added full fledged CLI to decline PRs on a massive scale

--- bitbucket-decline-prs-multiple.sh
+++ bitbucket-decline-prs-multiple.sh
@@ -1,28 +1,76 @@
 #!/bin/bash
 
-function printHelp {
-	echo "-projectKey				: Project Key"
-	echo "-repository				: Full Repository name"
-	echo "-userSlug				: User Slug or short user id"
-	echo "-password				: Self explanatory"
-	echo "-bitbucketServerUrl			: Bitbucket Server Url "
-	echo "-fromValue				: First Id of List of PRs to be declined"
-	echo "-toValue				: Final Id of Pr to be declined"
+function printUsage {
+	echo ""
+	echo "$0"
+	echo "--projectKey				: Project Key"
+	echo "--repository				: Full Repository name"
+	echo "--userSlug				: User Slug or short user id"
+	echo "--password				: Self explanatory"
+	echo "--bitbucketServerUrl			: Bitbucket Server Url "
+	echo "--fromValue				: First Id of List of PRs to be declined"
+	echo "--toValue				: Final Id of Pr to be declined"
 }
 
-if [ $# -eq 0 ]
-  then
+# The below function to extract version of Pull Request from json response has been copied from https://gist.github.com/cjus/1047794. , courtesy of Carlos Justiniano
+function getPrVersionFromJsonResponse {
+    temp=`echo $1 | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w version | cut -d":" -f2| sed -e 's/^ *//g' -e 's/ *$//g' `
+	version=${temp##*|}
+}
+
+while [ "$1" != "" ]; do
+    PARAM=`echo $1 | awk -F= '{print $1}'`
+    VALUE=`echo $1 | awk -F= '{print $2}'`
+    case $PARAM in
+        -h | --help)
+            printUsage
+            exit
+            ;;
+        --projectKey)
+            projectKey=$VALUE
+            ;;
+        --repository)
+            repository=$VALUE
+            ;;
+		--userSlug)
+            userSlug=$VALUE
+            ;;
+		--password)
+            password=$VALUE
+            ;;			
+		--bitbucketServerUrl)
+            bitbucketServerUrl=$VALUE
+            ;;
+		--fromValue)
+            fromValue=$VALUE
+            ;;		
+		--toValue)
+            toValue=$VALUE
+            ;;			
+        *)
+            echo "ERROR: unknown parameter \"$PARAM\""
+            printUsage
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+if [ -z "$projectKey" ] || [ -z "$repository" ] || [ -z "$userSlug" ] || [ -z "$password" ] || [ -z "$bitbucketServerUrl" ] || [ -z "$fromValue" ] || [ -z "$toValue" ] ; then
 	echo ""
-	echo "Since no arguments have been passed, printing help"
-	echo "" 
+	echo "Looks like one of the following inputs {projectKey, repository, userSlug, password, bitbucketServerUrl, fromValue, toValue} have not been supplied"
 	echo ""
-    printHelp
+	echo "See usage as mentioned below : "
+	printUsage
 	exit 1
 fi
 
 trap "exit" INT
-for i in `seq 1080 1200`; do 
-	echo "Declining Pull request $i "
-	curl -u "plugintester:Plugintester123" -X POST "https://git.dev.pega.io/rest/api/1.0/projects/LOADTEST/repos/prpc-platform/pull-requests/$i/decline?version=0" -H "X-Atlassian-Token: no-check"
+for i in `seq $fromValue $toValue`; do 
+	echo "Getting Version of Pull Request ID $i"
+	jsonResponse=$(curl -u "$userSlug:$password" -X GET "$bitbucketServerUrl/rest/api/1.0/projects/$projectKey/repos/$repository/pull-requests/$i")
+	getPrVersionFromJsonResponse $jsonResponse
+	
+	echo "Declining Pull request $i whose version is $version"
+	curl -u "$userSlug:$password" -X POST "$bitbucketServerUrl/rest/api/1.0/projects/$projectKey/repos/$repository/pull-requests/$i/decline?version=$version" -H "X-Atlassian-Token: no-check"
 done
-
